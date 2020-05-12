1ecbece4eeaf5cc0ef3f3afb8fa1c486f223aaa9
naraharishettysairam <58865992+naraharisettysairam@users.noreply.github.com>
2020-05-12 12:18:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,9 @@ variable "key_name" {}
 variable "public_subnet1_cidr" {}
 variable "public_subnet2_cidr" {}
 variable "public_subnet3_cidr" {}
+variable "public_subnet4_cidr" {}
 variable "public_subnet1_name" {}
 variable "public_subnet2_name" {}
 variable "public_subnet3_name" {}
+variable "public_subnet4_name" {}
 variable Main_Routing_Table {}
